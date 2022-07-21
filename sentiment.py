e177d112af8927a65131eb877c3d81eb0d67a528
codejunkie99 <52658655+codejunkie99@users.noreply.github.com>
2022-07-21 17:06:10 +0400

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -18,15 +18,7 @@ for index, row in df.iterrows():
         result = ' ' + "Positive" + ' ' + str(x) + ' ' + str(z)
     print(result)
 
-    #Work in Progress
-    #with open('result.csv', '+w', encoding='utf8', newline='') as x:
-            #thewriter = writer(x)
-            #header = ['Review', 'Sentiment', 'Polarity', 'Subjectivity']
-            #thewriter.writerow(header)
-            #info1 = [y, result,str(x),str(z)]
-            #thewriter.writerow(info1)
-            #print(info1)
-
+    
 
 
 
